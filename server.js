c2b22e2e1c15097f1c3e86f423c333673c0b855a
pitak pojwongmonkol <boyworking2@gmail.com>
2022-08-24 16:37:40 +0700

    update index.js use cors package

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.json({result: "ok", data: [1,2,3,4,5]});
+})
+
+app.get('/v1/service', (req, res) => {
+    res.json({msg: 'This is CORS-enabled for all origins!'});
 })
 
 app.listen(PORT, () => {
